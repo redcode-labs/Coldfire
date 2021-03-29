50ba9c0d0eef43ffdefd910aaeccf4ea7fbe17a8
Wintrmvte <redcode.labs@gmail.com>
2021-03-29 13:45:10 +0200

    Revert "Add SplitChunks()"
    This reverts commit 80233e5cc750d5c17ec83ac7884557a8432cd585.

--- coldfire.go
+++ coldfire.go
@@ -1661,27 +1661,6 @@ func SplitMultiSep(s string, seps []string) []string {
 	return fields
 }
 
-func SplitChunks(s string, chunkSize int) []string {
-    if chunkSize >= len(s) {
-        return []string{s}
-    }
-    var chunks []string
-    chunk := make([]rune, chunkSize)
-    len := 0
-    for _, r := range s {
-        chunk[len] = r
-        len++
-        if len == chunkSize {
-            chunks = append(chunks, string(chunk))
-            len = 0
-        }
-    }
-    if len > 0 {
-        chunks = append(chunks, string(chunk[:len]))
-    }
-    return chunks
-}
-
 /*
 func dialog(message, title string) {
 

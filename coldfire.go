f8410c02261e7c1590cbc9ebe71cda53b8782277
Filip Strajnar <filip.strajnar@gmail.com>
2021-06-30 16:10:24 +0200

    changed encryption

--- coldfire.go
+++ coldfire.go
@@ -1578,8 +1578,11 @@ func EncryptBytes(secret_message []byte, key []byte) []byte {
 	if err != nil {
 		println("Error occured, can't encrypt")
 	}
-	encoded := make([]byte, 0)
-	base64.StdEncoding.Encode(encoded, secret_message)
+
+	var writer bytes.Buffer
+	base64.NewEncoder(base64.StdEncoding,&writer)
+	encoded := writer.Bytes()
+	
 	IV := GenerateIV()
 	if len(encoded)%16 != 0 {
 		appending := make([]byte, len(encoded)%16)
@@ -1616,8 +1619,9 @@ func DecryptBytes(encrypted_message []byte, key []byte) []byte {
 	if found_null {
 		decrypted = decrypted[0:(first_null - 1)]
 	}
-
-	decoded := make([]byte, 0)
+	var writer bytes.Buffer
+	base64.NewDecoder(base64.StdEncoding,&writer)
+	decoded := writer.Bytes()
 	_, error := base64.StdEncoding.Decode(decoded, decrypted)
 	if error != nil {
 		println("Decoding failed")

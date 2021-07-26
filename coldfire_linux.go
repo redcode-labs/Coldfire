7f1f87a6dd895589c4adfc1ef7dbf6d41e227b27
Wintrmvte <redcode.labs@gmail.com>
2021-07-26 16:53:00 +0200

    sandboxDisk() returns false negative due to dependency error (fix required)

--- coldfire_linux.go
+++ coldfire_linux.go
@@ -128,19 +128,22 @@ func sandboxFilepath() bool {
 	return out != "none"
 }
 
+// HOTFIX - below function returns false negative, because 
+// installation of minio/pkg/disk package eats dick on absolutely every platform.
+// Rewriting this function using CmdOut() or 'syscall' package would be much appreciated :>
 func sandboxDisk(size int) bool {
 	v := false
-	d := "/"
-	di, _ := disk.GetInfo(d)
-	x := strings.Replace(humanize.Bytes(di.Total), "GB", "", -1)
-	x = strings.Replace(x, " ", "", -1)
-	z, err := strconv.Atoi(x)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if z < size {
-		v = true
-	}
+	//d := "/"
+	//di, _ := disk.GetInfo(d)
+	//x := strings.Replace(humanize.Bytes(di.Total), "GB", "", -1)
+	//x = strings.Replace(x, " ", "", -1)
+	//z, err := strconv.Atoi(x)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//if z < size {
+	//	v = true
+	//}
 	return v
 }
 

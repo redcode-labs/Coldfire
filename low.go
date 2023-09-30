55492331b954a6946d1e7736d6a8d4842aa2f002
wintrmvte <kubalutczyn@gmail.com>
2023-09-30 06:45:49 +0200

    Added IsEXE() and uncommented prototypes

--- low.go
+++ low.go
@@ -25,10 +25,10 @@ func VerifyELFMagic(fname string) bool {
 	var ident [16]uint8
 	f.ReadAt(ident[0:], 0)
 	Check(err)
-	if ident[0] != '\x7f' || ident[1] != 'E' || ident[2] != 'L' || ident[3] != 'F' {
+	if ident[0] == '\x7f' && ident[1] == 'E' && ident[2] == 'L' && ident[3] == 'F' {
 		return true
 	}
-	return false
+	return true
 }
 
 func IsELF(fname string) bool {
@@ -41,34 +41,55 @@ func IsELF(fname string) bool {
 	return true
 }
 
+func IsEXE(fname string) bool {
+	f := IOReader(fname) 
+	_, err := elf.NewFile(f)
+	Check(err)
+	if err != nil {
+		return false
+	}
+	var ident [16]uint8
+	f.ReadAt(ident[0:], 0)
+	Check(err)
+	if ident[0] == 'M' && ident[1] == 'Z' {
+		return true
+	}
+	return false
+
+}
+
 //func IsELFInfected(fname string) bool {
 //
 //}
 
 // Checks if an ELF file is designed for AMD x86_64 
-func IsELF64Bit(fname string) bool {
-	f := IOReader(fname)
-	elfile, err := elf.NewFile(f)
-	Check(err)
-	if (elfile.Class.String() == "ELFCLASS64" && elfile.Machine.String() == "EM_X86_64") {
-		return true
+func Is64Bit(fname string) bool {
+	if IsELF(fname) {
+		f := IOReader(fname)
+		elfile, err := elf.NewFile(f)
+		Check(err)
+		if (elfile.Class.String() == "ELFCLASS64" && elfile.Machine.String() == "EM_X86_64") {
+			return true
+		}
+		return false
+	} else {
+
 	}
-	return false
 }
 
-//func Caves(file string, min_size int) map[string]map[string]int {
-//	if IsELF(file) {
-//		elfile, err := elf.Open(file)
-//		Check(err)
-//		for _, sect := range elfile.Sections {
-//			data, _ := sect.Data()
-//			for off := 0; off < len(data); off++{
-//				if EqualBytes(data[off], 0x00) {
-//					
-//				}
-//			}
-//		}
-//	} else {
-//
-//	}
-//}
+func Caves(file string, min_size int) map[string]map[string]int {
+	if IsELF(file) {
+		elfile, err := elf.Open(file)
+		Check(err)
+		for _, sect := range elfile.Sections {
+			data, _ := sect.Data()
+			for off := 0; off < len(data); off++{
+				if EqualBytes(data[off], 0x00) {
+					
+				}
+			}
+		}
+	} else {
+
+	}
+}
